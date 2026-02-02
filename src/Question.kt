5af34d32c0667cf90b8afe5d085931d68d104ee4
mrultimatum07-alt <mrultimatum07@gmail.com>
2026-02-02 14:14:08 +0300

    Make a reusable class with generics

---
+++ src/Question.kt
@@ -0,0 +1,28 @@
+class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: String
+)
+
+fun main() {
+    val  question1 = Question<String>(
+        "Речка спятила с ума - По домам пошла сама. ___",
+        "Водопровод",
+        "medium"
+    )
+    val question2 = Question<Boolean>(
+        "Небо зелёное. Правда или ложь",
+        false,
+        "easy"
+    )
+    val question3 = Question<Int>(
+        "Сколько дней между полнолуниями",
+        28,
+        "hard"
+    )
+    println(question1.answer)
+    println(question2.answer)
+    println(question3.answer)
+
+
+}
